25630bd04c4a08db50b92b3664fea0278fb5c9bf
Herman <evanaherman@gmail.com>
2023-03-07 00:29:37 -0800

    added images to projects folder

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -6,42 +6,42 @@ const Portfolio = () => {
     const Project = [
         {
             title:"Little Library",
-            image:"",
+            image:"little-library",
             imageAlt:"image of little library app",
             repo:"https://github.com/HowardLee2022/Little-Library",
             url:"https://littlelibrary.herokuapp.com/"
         },
         {
             title:"Pokemon Deck Generator",
-            image:"",
+            image:"pokemon-deck-generator",
             imageAlt:"image of pokemon deck generator app",
             repo:"https://github.com/JJackielee/pokemon-deck-generator/",
             url:"https://jjackielee.github.io/pokemon-deck-generator/"
         },
         {
             title:"Note Taker",
-            image:"",
+            image:"notezfordayz",
             imageAlt:"image of note taker app",
             repo:"https://github.com/hermanea/note-taker",
             url:"https://notezfordayz.herokuapp.com/"
         },
         {
             title:"Quiz Time",
-            image:"",
+            image:"quiz-time",
             imageAlt:"image of quiz time app",
             repo:"https://github.com/hermanea/quiz-time",
             url:"https://hermanea.github.io/quiz-time/"
         },
         {
             title:"Work Scheduler",
-            image:"",
+            image:"work-scheduler",
             imageAlt:"image of work scheduler app",
             repo:"https://github.com/hermanea/work-scheduler-challenge05",
             url:"https://hermanea.github.io/work-scheduler-challenge05/"
         },
         {
             title:"Five Day Forecast",
-            image:"",
+            image:"five-day-forecast",
             imageAlt:"image of five day forecast app",
             repo:"https://github.com/hermanea/five-day-forecast-challenge06",
             url:"https://hermanea.github.io/five-day-forecast-challenge06/"
